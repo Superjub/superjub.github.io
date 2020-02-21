979dfcf41c81144a7236174390e5d282583dac2b
Bradley Pick <bcpick1995@gmail.com>
2020-02-21 13:45:29 +0000

    new project. Various fixed. Styling tweaks.

--- js/index.js
+++ js/index.js
@@ -11,22 +11,30 @@ AngularInterface.controller("window", ['$scope', '$element', '$window', '$rootSc
 
 AngularInterface.controller("portfolio", ['$scope', '$element', '$window', '$rootScope', function ($s, $e, $w, $root) {
     $s.projects = [{   
-        name: 'Posession',
-        image_url: 'images/posession-screenshot.png',
-        description: 'Posession is an eery RPG where you play as a young female protagonist in strange circumstances.',
+        name: "Posession",
+        image_url: 'images/projects/posession/posession-screenshot.png',
+        description: 'Posession is an eerie RPG where you play as a young female protagonist in strange circumstances.',
         download_url: null
     },
     {
-        name: 'Clown Calamity',
+        name: "Clown Calamity",
         image_url: '',
         description: 'Clown Calamity is a vehicle-based game where you dodge wacky obstacles while surviving as long as possible.',
         download_url: null
+    },
+    {
+        name: "Bradley's Blackjack",
+        image_url: "images/projects/blackjack/blackjack_image_2.png",
+        description: "A terminal-based pure text game created using C++ featuring standard Blackjack rules as well as a points system to track score.",
+        download_url: "https://github.com/Superjub/blackjack_vs_console_2.0/archive/master.zip"
     }];
 
-    $s.download = function ($event, project) {
-        if(project.download_url === null) {
-
+    $s.download = function (url) {
+        if (url === null) {
+            return;
         }
+        console.log(url);
+        $w.open(url);
     }
 
 }]);
